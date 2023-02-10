c46a462b6184e81d71351eca3ada0a766a7f0d2f
LabUser <LabUser@DESKTOP-BT4131D>
2023-02-10 02:56:32 +0000

    Add project files.

---
+++ SchedulingService/Views/LoginForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace SchedulingService
+{
+	partial class LoginForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.usernameLabel = new System.Windows.Forms.Label();
+			this.passwordLabel = new System.Windows.Forms.Label();
+			this.username = new System.Windows.Forms.TextBox();
+			this.password = new System.Windows.Forms.TextBox();
+			this.loginButton = new System.Windows.Forms.Button();
+			this.welcomeLabel = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// usernameLabel
+			// 
+			this.usernameLabel.AutoSize = true;
+			this.usernameLabel.Location = new System.Drawing.Point(274, 151);
+			this.usernameLabel.Name = "usernameLabel";
+			this.usernameLabel.Size = new System.Drawing.Size(61, 13);
+			this.usernameLabel.TabIndex = 0;
+			this.usernameLabel.Text = "Username: ";
+			// 
+			// passwordLabel
+			// 
+			this.passwordLabel.AutoSize = true;
+			this.passwordLabel.Location = new System.Drawing.Point(274, 190);
+			this.passwordLabel.Name = "passwordLabel";
+			this.passwordLabel.Size = new System.Drawing.Size(53, 13);
+			this.passwordLabel.TabIndex = 1;
+			this.passwordLabel.Text = "Password";
+			// 
+			// username
+			// 
+			this.username.Location = new System.Drawing.Point(374, 148);
+			this.username.Name = "username";
+			this.username.Size = new System.Drawing.Size(103, 20);
+			this.username.TabIndex = 2;
+			// 
+			// password
+			// 
+			this.password.Location = new System.Drawing.Point(374, 187);
+			this.password.Name = "password";
+			this.password.PasswordChar = '*';
+			this.password.Size = new System.Drawing.Size(103, 20);
+			this.password.TabIndex = 3;
+			// 
+			// loginButton
+			// 
+			this.loginButton.Location = new System.Drawing.Point(277, 221);
+			this.loginButton.Name = "loginButton";
+			this.loginButton.Size = new System.Drawing.Size(200, 23);
+			this.loginButton.TabIndex = 4;
+			this.loginButton.Text = "Login";
+			this.loginButton.UseVisualStyleBackColor = true;
+			// 
+			// welcomeLabel
+			// 
+			this.welcomeLabel.AutoSize = true;
+			this.welcomeLabel.Location = new System.Drawing.Point(12, 9);
+			this.welcomeLabel.Name = "welcomeLabel";
+			this.welcomeLabel.Size = new System.Drawing.Size(228, 13);
+			this.welcomeLabel.TabIndex = 5;
+			this.welcomeLabel.Text = "Welcome to scheduling services, please log-in.";
+			// 
+			// LoginForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.welcomeLabel);
+			this.Controls.Add(this.loginButton);
+			this.Controls.Add(this.password);
+			this.Controls.Add(this.username);
+			this.Controls.Add(this.passwordLabel);
+			this.Controls.Add(this.usernameLabel);
+			this.Name = "LoginForm";
+			this.Text = "Log-In";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label usernameLabel;
+		private System.Windows.Forms.Label passwordLabel;
+		private System.Windows.Forms.TextBox username;
+		private System.Windows.Forms.TextBox password;
+		private System.Windows.Forms.Button loginButton;
+		private System.Windows.Forms.Label welcomeLabel;
+	}
+}
+
